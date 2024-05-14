d174bc61bb1a911319d84234e23280986323c888
valeriin0ry <153493936+valeriin0ry@users.noreply.github.com>
2024-05-14 10:59:33 +0300

    Add files via upload

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -9,7 +9,7 @@ pygame.draw.polygon(ekraani_pind, (169, 169, 169), [(0, 500), (0, 300), (100, 50
 pygame.draw.ellipse(ekraani_pind, (0, 100, 0), [350, 200, 100, 150], 0)
 pygame.draw.rect(ekraani_pind, (0, 255, 0), [0, 500, 800, 100])
 pygame.draw.arc(ekraani_pind, (255, 255, 255), [100, 0, 600, 100], 0, 3.14, 20)
-pilt1 = pygame.image.load("audi.jpg")
+pilt1 = pygame.image.load("audi777.png")
 pilt1 = pygame.transform.scale(pilt1, (200, 200))
 ekraani_pind.blit(pilt1, (600, 400))
 tekst = "Valerii Pygame"
